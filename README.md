a4ac5e95caf3b98be3acf3dd663559c63ba913f7
Boris Djoum <tchatchuengboris@gmail.com>
2024-05-05 20:22:21 +0100

    chatbot improved

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # Bao_Chatbot
+
+## Presentation of the project
+
+Bao is a multilingual chatbot created from scratch (without the use of a pre-trained model) specialising in the field of agriculture. Its aim is to help farmers find out more about the right conditions for a specific crop to grow properly. Although it is specialised in this field, it can also answer open questions or conversational questions.
+
+
+## Version constraints
+- Python*: Approximately 3.7 to 3.12
+
+
+## Bao_chatbot project structure
+```
+Bao_chatbot/
+│
+├── datasets/
+│ ├── intents.json
+│ ├── intentsfr.json
+│ ├── links.txt
+|
+│── bao_env/
+|
+|
+├── my_app_Flask/
+│ ├── templates/
+│ │ └── chat.html
+│ ├── app.py
+│ │   
+│ ├── chat.py   
+│
+├── my_models/
+│   
+│
+├── requirements.txt
+│
+└── train.ipynb
+```
+
+
+## Explanation of the structure :
+
+- datasets/ : Contains mainly the datasets used to train the models.
+
+    - intents.json : Contains mainly data in English.
+
+    - intentsfr.json : Contains mainly French-language data.
+
+    - links.txt : Contains the links I used to create this dataset.
+
+- bao_env/ : is the virtual environment we created to install all the dependencies needed to launch the project.
+
+- my_app_Flask/ : This directory contains the chat interface, as well as the api and the flask api that we've integrated into our interface. :
+
+    - templates/ : Contains the single chat.html file, which is our chat interface.
+    - app.py/ : This is our flask api.
+    - chat.py : Contains the useful functions for interacting with the chat.
+    
+- my_models : Contains the models trained in the train.ipynb file.
+
+- requirements.txt : A file listing the project's dependencies. It is used to install all the dependencies in our virtual environment.
+
+
+- train.ipynb: file containing the training and evaluation of the model.
+
+
+## Procedure for launching Bao_chatbot
+
+To start you need to create a virtual environment in the project directory using the following:
+
+- On Linux:
+
+```console
+python -m venv bao_env
+```
+
+- On Windows:
+
+```console
+py -m venv bao_env
+```
+
+Then you need to activate the virtual environment using the following command:
+
+- On Linux:
+
+```console
+source bao_env/bin/activate
+```
+
+- On Windows:
+
+```console
+bao_env\Scripts\activate
+```
+
+Then install all the python libraries with the specified version using the command :
+
+```console
+pip install -r requirements.txt
+```
+
+Finally, to launch the application, go into the ``my_app_Flask`` folder and run the ``app.py`` file.
---
+++ my_app_Flask/chat.py
@@ -0,0 +1,224 @@
+import json 
+import string
+import numpy as np
+from tensorflow import keras
+
+from nltk.stem import WordNetLemmatizer 
+import nltk
+import colorama 
+colorama.init()
+from colorama import Fore, Style, Back
+from langdetect import detect
+from googletrans import Translator
+from translate import Translator
+nltk.download("punkt")
+nltk.download("wordnet")
+import random
+
+
+lemmatizer = WordNetLemmatizer()
+
+with open("datasets/intents.json") as file:
+    data = json.load(file)
+    
+    
+with open("datasets/intentsfr.json") as file:
+    datafr = json.load(file)    
+    
+    
+model = keras.models.load_model('my_models/chat_model')
+modelfr = keras.models.load_model('my_models/chat_modelfr')
+
+
+
+words = [] #For Bow model/ vocabulary for patterns
+classes = [] #For Bow  model/ vocabulary for tags
+data_X = [] #For storing each pattern
+data_y = [] #For storing tag corresponding to each pattern in data_X 
+# Iterating over all the intents
+
+
+wordsfr = [] #For Bow model/ vocabulary for patterns
+classesfr = [] #For Bow  model/ vocabulary for tags
+datafr_X = [] #For storing each pattern
+datafr_y = [] #For storing tag corresponding to each pattern in data_X 
+# Iterating over all the intents
+
+
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        tokens = nltk.word_tokenize(pattern) # tokenize each pattern 
+        words.extend(tokens) #and append tokens to words
+        data_X.append(pattern) #appending pattern to data_X
+        data_y.append(intent["tag"]) ,# appending the associated tag to each pattern 
+    
+    # adding the tag to the classes if it's not there already 
+    if intent["tag"] not in classes:
+        classes.append(intent["tag"])
+
+
+
+for intent in datafr["intents"]:
+    for pattern in intent["patterns"]:
+        tokens = nltk.word_tokenize(pattern) # tokenize each pattern 
+        wordsfr.extend(tokens) #and append tokens to words
+        datafr_X.append(pattern) #appending pattern to data_X
+        datafr_y.append(intent["tag"]) ,# appending the associated tag to each pattern 
+    
+    # adding the tag to the classes if it's not there already 
+    if intent["tag"] not in classesfr:
+        classesfr.append(intent["tag"])
+
+
+# initializing lemmatizer to get stem of words
+lemmatizer = WordNetLemmatizer()
+
+# lemmatize all the words in the vocab and convert them to lowercase
+# if the words don't appear in punctuation
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in string.punctuation]
+# sorting the vocab and classes in alphabetical order and taking the # set to ensure no duplicates occur
+words = sorted(set(words))
+classes = sorted(set(classes))
+
+
+# lemmatize all the words in the vocab and convert them to lowercase
+# if the words don't appear in punctuation
+wordsfr = [lemmatizer.lemmatize(word.lower()) for word in wordsfr if word not in string.punctuation]
+# sorting the vocab and classes in alphabetical order and taking the # set to ensure no duplicates occur
+wordsfr = sorted(set(wordsfr))
+classesfr = sorted(set(classesfr))
+
+
+#7 Preprocessing the Input
+
+def clean_text(text): 
+  tokens = nltk.word_tokenize(text)
+  tokens = [lemmatizer.lemmatize(word) for word in tokens]
+  return tokens
+
+def bag_of_words(text, vocab): 
+  tokens = clean_text(text)
+  bow = [0] * len(vocab)
+  for w in tokens: 
+    for idx, word in enumerate(vocab):
+      if word == w: 
+        bow[idx] = 1
+  return np.array(bow)
+
+def pred_class(text, vocab, labels): 
+  bow = bag_of_words(text, vocab)
+  result = model.predict(np.array([bow]))[0] #Extracting probabilities
+  thresh = 0.5
+  y_pred = [[indx, res] for indx, res in enumerate(result) if res > thresh]
+  y_pred.sort(key=lambda x: x[1], reverse=True) #Sorting by values of probability in decreasing order
+  return_list = []
+  for r in y_pred:
+    return_list.append(labels[r[0]]) #Contains labels(tags) for highest probability 
+  return return_list
+
+def get_response(intents_list, intents_json): 
+  if len(intents_list) == 0:
+    result = "Sorry! I don't understand."
+  else:
+    tag = intents_list[0]
+    list_of_intents = intents_json["intents"]
+    for i in list_of_intents: 
+      if i["tag"] == tag:
+        result = random.choice(i["responses"])
+        break
+  return result
+
+
+## For data in french
+
+def clean_textfr(text): 
+  tokens = nltk.word_tokenize(text)
+  tokens = [lemmatizer.lemmatize(word) for word in tokens]
+  return tokens
+
+def bag_of_wordsfr(text, vocab): 
+  tokens = clean_textfr(text)
+  bow = [0] * len(vocab)
+  for w in tokens: 
+    for idx, word in enumerate(vocab):
+      if word == w: 
+        bow[idx] = 1
+  return np.array(bow)
+
+def pred_classfr(text, vocab, labels): 
+  bow = bag_of_wordsfr(text, vocab)
+  result = modelfr.predict(np.array([bow]))[0] #Extracting probabilities
+  thresh = 0.5
+  y_pred = [[indx, res] for indx, res in enumerate(result) if res > thresh]
+  y_pred.sort(key=lambda x: x[1], reverse=True) #Sorting by values of probability in decreasing order
+  return_list = []
+  for r in y_pred:
+    return_list.append(labels[r[0]]) #Contains labels(tags) for highest probability 
+  return return_list
+
+def get_responsefr(intents_list, intents_json): 
+  if len(intents_list) == 0:
+    result = "Désolé! Je ne comprends pas."
+  else:
+    tag = intents_list[0]
+    list_of_intents = intents_json["intents"]
+    for i in list_of_intents: 
+      if i["tag"] == tag:
+        result = random.choice(i["responses"])
+        break
+  return result
+
+
+def chat():
+   
+    
+    while True:
+        print(Fore.LIGHTBLUE_EX + "User: " + Style.RESET_ALL, end="")
+        inp = input()
+        if inp.lower() == "quit":
+            break
+
+        intents = pred_class(inp, words, classes)
+        result = get_response(intents, data)
+        print(Fore.GREEN + "Bao:" + Style.RESET_ALL , result)
+
+        # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
+
+
+def detect_language(text):
+    return detect(text)
+
+def translate_text(text, target_language):
+  translator = Translator(to_lang=target_language)
+  translation = translator.translate(text)
+  return translation
+
+def chatbot(message):
+    user_language = detect_language(message)
+    print("User Language:", user_language)  # Vérifier la langue détectée
+
+    if user_language == "fr":
+      intents = pred_classfr(message, wordsfr, classesfr)
+      result = get_responsefr(intents, datafr)
+
+    elif user_language =="en" or message == "Hello":
+      intents = pred_class(message, words, classes)
+      result = get_response(intents, data)
+      
+      
+    else:
+      translated_message = translate_text(message, 'en')  # Traduire le message de l'utilisateur en anglais
+      
+      # Utiliser votre modèle de chatbot entraîné en anglais pour générer une réponse en anglais
+      intents = pred_class(translated_message, words, classes)
+      print("English Intents:", intents)  # Vérifier les classes prédites en anglais
+
+      result = get_response(intents, data)
+      print("English Result:", result)  # Vérifier la réponse générée en anglais
+
+      
+      result = translate_text(result, user_language)  # Traduire la réponse en langage de l'utilisateur
+      print("Translated Result:", result)  # Vérifier la réponse traduite
+      
+    return result
